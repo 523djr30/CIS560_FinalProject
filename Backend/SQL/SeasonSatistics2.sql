2cf748bdc5e91a76fb7350c28abd37ae8dbd67df
JAK244 <111782318+JAK244@users.noreply.github.com>
2024-11-21 22:10:58 -0600

    Add files via upload

---
+++ Backend/SQL/SeasonSatistics2.sql
@@ -0,0 +1,45 @@
+WITH TeamSeasonStats AS (
+    SELECT T.Name AS TeamName, S.SeasonId,
+        COUNT(CASE WHEN (M.HomeTeamId = T.TeamId AND 
+						(SELECT SUM(PS.Points) 
+                         FROM Football.PlayerMatchStats PS 
+                         WHERE PS.MatchId = M.MatchId AND PS.TeamId = M.HomeTeamId) > 
+                        (SELECT SUM(PS.Points) 
+                         FROM Football.PlayerMatchStats PS 
+                         WHERE PS.MatchId = M.MatchId AND PS.TeamId = M.AwayTeamId)) 
+                     OR 
+                       (M.AwayTeamId = T.TeamId AND 
+                        (SELECT SUM(PS.Points) 
+                         FROM Football.PlayerMatchStats PS 
+                         WHERE PS.MatchId = M.MatchId AND PS.TeamId = M.AwayTeamId) > 
+                        (SELECT SUM(PS.Points) 
+                         FROM Football.PlayerMatchStats PS 
+                         WHERE PS.MatchId = M.MatchId AND PS.TeamId = M.HomeTeamId)) 
+                  THEN 1 
+             END) AS Wins,
+        COUNT(CASE WHEN (M.HomeTeamId = T.TeamId AND 
+                        (SELECT SUM(PS.Points) 
+                         FROM Football.PlayerMatchStats PS 
+                         WHERE PS.MatchId = M.MatchId AND PS.TeamId = M.HomeTeamId) < 
+                        (SELECT SUM(PS.Points) 
+                         FROM Football.PlayerMatchStats PS 
+                         WHERE PS.MatchId = M.MatchId AND PS.TeamId = M.AwayTeamId)) 
+                     OR 
+                       (M.AwayTeamId = T.TeamId AND 
+                        (SELECT SUM(PS.Points) 
+                         FROM Football.PlayerMatchStats PS 
+                         WHERE PS.MatchId = M.MatchId AND PS.TeamId = M.AwayTeamId) < 
+                        (SELECT SUM(PS.Points) 
+                         FROM Football.PlayerMatchStats PS 
+                         WHERE PS.MatchId = M.MatchId AND PS.TeamId = M.HomeTeamId)) 
+                  THEN 1 
+             END) AS Losses
+    FROM Football.Match M
+    INNER JOIN Football.Team T ON M.HomeTeamId = T.TeamId OR M.AwayTeamId = T.TeamId
+    INNER JOIN Football.Season S ON M.Date BETWEEN S.StartDate AND S.EndDate
+    WHERE S.SeasonId = @SeasonId  -- Parameter: Season
+          AND T.Name = @TeamName  -- Parameter: Team Name
+    GROUP BY T.Name, S.SeasonId
+)
+SELECT TeamName, SeasonId AS Season, Wins, Losses
+FROM TeamSeasonStats;
